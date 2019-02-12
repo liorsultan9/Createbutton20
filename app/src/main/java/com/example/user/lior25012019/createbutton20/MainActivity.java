1f87617d9bd74349aee9897e8772b15de6720f24
liorsultan9 <41189009+liorsultan9@users.noreply.github.com>
2019-02-12 13:59:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user/lior25012019/createbutton20/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.lior25012019.createbutton20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
