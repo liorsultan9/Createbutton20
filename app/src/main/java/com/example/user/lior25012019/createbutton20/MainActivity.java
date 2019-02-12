7ed46c709f107f397f3609e5a43a0c788f9dcfd8
liorsultan9 <41189009+liorsultan9@users.noreply.github.com>
2019-02-12 21:14:27 +0200

    yes

--- app/src/main/java/com/example/user/lior25012019/createbutton20/MainActivity.java
+++ app/src/main/java/com/example/user/lior25012019/createbutton20/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.user.lior25012019.createbutton20;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+  private EditText userInput;
+  private Button button;
+  private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userInput=(EditText) findViewById(R.id.editText);
+        button =(Button) findViewById(R.id.button);
+        textView=(TextView) findViewById(R.id.textView);
     }
 }
